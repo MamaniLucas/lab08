28e4075fade0d4e84f855ad63b390112f7f6e2b3
Alumno <Alumno@LAB1509-08>
2023-11-03 17:33:01 -0500

    Agregar archivos de proyecto.

---
+++ lab08/lab08/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace lab08
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
